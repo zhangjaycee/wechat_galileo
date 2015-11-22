4e450c2a6911cfb9a28823f3d991645bc34f8272
jczhang <zhjcyx@gmail.com>
2015-11-22 00:38:58 -0800

    new branch

--- galileo/views.py
+++ galileo/views.py
@@ -6,6 +6,21 @@ from django.utils.encoding import smart_str, smart_unicode
 import hashlib
 from xml.etree import ElementTree as etree
 import getinfo
+from wechat_sdk import WechatBasic
+from wechat_sdk.exceptions import ParseError
+from wechat_sdk.messages import TextMessage
+
+
+WECHAT_TOKEN = 'jcgalileo'
+AppID = ''
+AppSecret = ''
+
+# 实例化 WechatBasic
+wechat_instance = WechatBasic(
+    token=WECHAT_TOKEN
+    #appid=AppID,
+    #appsecret=AppSecret
+)
 
 @csrf_exempt
 def index(request):
@@ -13,24 +28,33 @@ def index(request):
         response=HttpResponse(checkSignature(request))
         return response
     else:
-       xmlstr = smart_str(request.body)
-       xml = etree.fromstring(xmlstr)
-
-       ToUserName = xml.find('ToUserName').text
-       FromUserName = xml.find('FromUserName').text
-       CreateTime = xml.find('CreateTime').text
-       MsgType = xml.find('MsgType').text
-       Content = xml.find('Content').text
-       MsgId = xml.find('MsgId').text
-       info = getinfo.getinfo(Content)
-       reply_xml = """<xml>
-       <ToUserName><![CDATA[%s]]></ToUserName>
-       <FromUserName><![CDATA[%s]]></FromUserName>
-       <CreateTime>%s</CreateTime>
-       <MsgType><![CDATA[text]]></MsgType>
-       <Content><![CDATA[%s]]></Content>
-       </xml>"""%(FromUserName,ToUserName,CreateTime,Content + info)
-       return HttpResponse(reply_xml)
+        # 解析本次请求的 XML 数据
+        try:
+            wechat_instance.parse_data(data=request.body)
+        except ParseError:
+            return HttpResponseBadRequest('Invalid XML Data')
+ 
+        # 获取解析好的微信请求信息
+        message = wechat_instance.get_message()
+ 
+        # 关注事件以及不匹配时的默认回复
+        response = wechat_instance.response_text(
+            content = (
+                '感谢您的关注！\n回复【功能】查看支持的功能'
+            )
+        )
+        if isinstance(message, TextMessage):
+            # 当前会话内容
+            content = message.content.strip()
+            if content == '功能':
+                reply_text = (
+                        '目前支持的功能：\n1...'
+                        '2...\n'
+                )
+ 
+            response = wechat_instance.response_text(content=reply_text)
+ 
+        return HttpResponse(response, content_type="application/xml")
 
 def checkSignature(request):
     signature=request.GET.get('signature',None)
