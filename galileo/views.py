c1423db47fc81962c9773db3d6e618386d986bb9
jczhang <zhjcyx@gmail.com>
2015-12-06 14:02:55 +0800

    finished

--- galileo/views.py
+++ galileo/views.py
@@ -33,10 +33,12 @@ wechat_instance = WechatBasic(
 )
 
 light_flag = 0
+detect_flag = 0
 
 @csrf_exempt
 def index(request):
     global light_flag
+    global detect_flag
     if request.method=='GET':
         response=HttpResponse(checkSignature(request))
         return response
@@ -45,17 +47,25 @@ def index(request):
         xml = etree.fromstring(xmlstr)
         temprature = xml.find('temprature')
         light = xml.find('light')
+        dangerous = xml.find('dangerous')
         if temprature != None:
             fp_tmp.seek(0,2)
             fp_tmp.write(temprature.text)
-            if temprature.text == '1234':
-                fp = open('galileo.jpg','rb')	
-                upload_info = wechat_instance.upload_media("image", fp)
-                image_id = upload_info['media_id']
-                wechat_instance.send_image_message(MY_OPENID, image_id)
+            #if temprature.text == '1234':
+            #    fp = open('galileo.jpg','rb')	
+            #    upload_info = wechat_instance.upload_media("image", fp)
+            #    image_id = upload_info['media_id']
+            #    wechat_instance.send_image_message(MY_OPENID, image_id)
             return HttpResponse("0")
         if light != None:
             return HttpResponse(str(light_flag))
+        if dangerous != None and detect_flag == 1:
+            fp = open('galileo_dangerous.jpg','rb')	
+            upload_info = wechat_instance.upload_media("image", fp)
+            image_id = upload_info['media_id']
+            wechat_instance.send_text_message(MY_OPENID, u"监控报警!请注意!")
+            wechat_instance.send_image_message(MY_OPENID, image_id)
+            return HttpResponse("ok recieved!")
         #获取open_id
         #open_id = xml.find('FromUserName')
         #if open_id != None:
@@ -83,16 +93,17 @@ def index(request):
             content = message.content.strip()
             if content == u'功能':
                 reply_text = (
-                        '目前支持的功能：\n1.回复"温度"，查询113室温\n'
-                        '2.回复"拍照"，偷窥jc的生活状态\n'
+                        '目前支持的功能：\n1.回复"温度"，查询室温\n'
+                        '2.回复"状态"，查看当前屋内状态\n'
                         '3.回复"开灯"或"关灯"或"光控"，控制家中灯的亮灭\n'
+                        '4.回复"打开监控"，当视频监控出现异常，自动报警；回复"关闭监控"，关闭报警功能\n'
                 )
                 response = wechat_instance.response_text(content=reply_text)
             elif content == u'温度':
                 fp_tmp.seek(-4, 2)
-                reply_text = fp_tmp.read(4)
+                reply_text = "当前室温为" + fp_tmp.read(4) + "°C"
                 response = wechat_instance.response_text(content=reply_text)
-            elif content == u'拍照':
+            elif content == u'状态':
                 #response = wechat_instance.response_text(content="debug....")
                 fp = open('galileo.jpg','rb')	
                 upload_info = wechat_instance.upload_media("image", fp)
@@ -107,6 +118,12 @@ def index(request):
             elif content == u'光控':
                 light_flag = 2
                 response = wechat_instance.response_text(content='灯已调为光控模式,室内较暗时自动打开')
+            elif content == u'打开监控':
+                detect_flag = 1
+                response = wechat_instance.response_text(content='视频监控已经打开，若发现异常将自动报警')
+            elif content == u'关闭监控':
+                detect_flag = 0
+                response = wechat_instance.response_text(content='视频监控已经关闭')
                 
         return HttpResponse(response, content_type="application/xml")
 
